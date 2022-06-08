d4430d2d22c2f1d9781e873d2523c71b4ee68ea5
svilen iordanov <svilenborianov@gmail.com>
2022-06-08 10:16:07 +0200

    Add accessory attac page

--- src/Controllers/AccessoryControler.js
+++ src/Controllers/AccessoryControler.js
@@ -2,4 +2,6 @@ var router = require("express").Router();
 
 router.get("/create", (request, response) => response.render("createAccessory"));
 
+// add id
+router.get("/attach", (request, response) => response.render("attachAccessory"));
 module.exports = router;
