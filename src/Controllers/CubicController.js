01380b94b69f5fbc9db6c618c32feaae02cea545
svilen iordanov <svilenborianov@gmail.com>
2022-06-13 11:16:44 +0200

    Visualize accessoaries in details page

--- src/Controllers/CubicController.js
+++ src/Controllers/CubicController.js
@@ -16,8 +16,7 @@ router.post("/create", (request, response) => {
 });
 
 router.get("/details/:Id", async (request, response) => {
-    var cube = await cubeService.GetById(request.params.Id).lean();
-
+    var cube = await cubeService.GetById(request.params.Id).lean().populate("accessories");
     response.render("details", { cube });
 });
 // not implemented
