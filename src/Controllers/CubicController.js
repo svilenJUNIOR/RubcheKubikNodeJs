9b2433a0d46c958b42aaa805bd129a8db485893b
svilen iordanov <svilenborianov@gmail.com>
2022-06-14 11:54:38 +0200

    Edit cube

--- src/Controllers/CubicController.js
+++ src/Controllers/CubicController.js
@@ -4,13 +4,17 @@ var accessoryService = require("../Services/AccessoryService");
 var engine = require("../Services/Engine");
 
 router.get("/create", (request, response) => response.render("create"));
-router.get("/edit", (request, response) => response.render("editCubePage"));
 router.get("/delete", (request, response) => response.render("deleteCubePage"));
 
 router.get("/details/:Id", async (request, response) => {
     var cube = await cubeService.GetById(request.params.Id).lean().populate("accessories");
     response.render("details", { cube });
 });
+
+router.get("/edit/:Id", async (request, response) => {
+    var cube = await cubeService.GetById(request.params.Id).lean();
+    response.render("editCubePage", { cube });
+}); 
 
 router.get("/attach/:Id", async (request, response) => {
     var cube = await cubeService.GetById(request.params.Id).lean();
@@ -20,5 +24,10 @@ router.get("/attach/:Id", async (request, response) => {
 
 router.post("/create", (request, response) => engine.AddCube(request, response));
 router.post("/attach/:Id", async (request, response) => engine.AttachAccessoryToCube(request, response));
+
+router.post("/edit/:Id", async (request, response) => {
+   await cubeService.Edit(request.params.Id, request.body);
+   response.redirect('/');
+});
 
 module.exports = router;
--- src/Services/CubicService.js
+++ src/Services/CubicService.js
@@ -3,3 +3,4 @@ var Cube = require("../../Data/Models/Cube");
 exports.GetById = (Id) => Cube.findById(Id);
 exports.AddNewCube = (request, response) => Cube.create(request.body)
 exports.getAll = async () => Cube.find().lean();
+exports.Edit = (Id, Data) => Cube.findByIdAndUpdate(Id, Data);
